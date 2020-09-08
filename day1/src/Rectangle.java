093183bc1164d2632612dcb9635e212732861e4d
omgbqh98 <69282402+omgbqh98@users.noreply.github.com>
2020-09-08 16:19:17 +0700

    shinhchunhat:

---
+++ day1/src/Rectangle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        int S;
+        Scanner scanner =new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+S=a*b;
+        System.out.println("diện tích hình chữ nhật là: " +S);
+    }
+}
