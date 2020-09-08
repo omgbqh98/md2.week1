04753a5eca38989832f44682a4f6734e6a1145ca
omgbqh98 <69282402+omgbqh98@users.noreply.github.com>
2020-09-08 16:07:03 +0700

    khaibaobien

--- day1/src/SystemTime.java
+++ day1/src/SystemTime.java
@@ -1,8 +1,11 @@
+
+
 import java.util.Date;
 
 public class SystemTime {
     public static void main(String[] args) {
         Date now = new Date();
-        System.out.println("date now: "+ now);
+        System.out.println(now);
     }
+
 }
---
+++ day1/src/khaiBaoBien.java
@@ -0,0 +1,10 @@
+public class khaiBaoBien {
+    public static void main(String[] args) {
+        int i = 20;
+        double f =20.5;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "hà nội";
+    }
+}
